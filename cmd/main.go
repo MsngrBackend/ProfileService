12be862fc629ceac0e1c05fb91961746f51f338b
danvlsv <daniil_vlasov_03@mail.ru>
2026-03-09 22:09:08 +0500

    chore: added basic CORS setup

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/rs/cors"
+
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
@@ -15,7 +17,6 @@ import (
 )
 
 func main() {
-
 	if err := godotenv.Load(); err != nil {
 		log.Println("no .env file found")
 	}
@@ -43,6 +44,13 @@ func main() {
 	h := delivery.NewHandler(profileUC, contactsUC, privacyUC, favUC, notifUC)
 	router := h.NewRouter()
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: false,
+	})
+
 	log.Println("Profile service running on :8082")
-	log.Fatal(http.ListenAndServe(":8082", router))
+	log.Fatal(http.ListenAndServe(":8082", c.Handler(router)))
 }
